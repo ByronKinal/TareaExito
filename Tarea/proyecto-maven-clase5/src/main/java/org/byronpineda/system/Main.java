a93e49655a889e352c9e8b935a73947203206194
ByronKinal <bpineda-2024427@kinal.edu.gt>
2025-07-13 08:18:27 -0600

    creacion de la tarea

--- Tarea/proyecto-maven-clase5/src/main/java/org/byronpineda/system/Main.java
+++ Tarea/proyecto-maven-clase5/src/main/java/org/byronpineda/system/Main.java
@@ -1,41 +1,70 @@
 package org.byronpineda.system;
 
+import java.io.InputStream;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
+import org.byronpineda.controller.InicioController;
+import org.fundacionkinal.controller.ComprasController;
+import org.fundacionkinal.controller.EmpleadoController;
+import org.fundacionkinal.controller.Factura2Controller;
+import org.fundacionkinal.controller.FacturaController;
+import org.fundacionkinal.controller.LoginController;
+import org.fundacionkinal.controller.MenuAdminController;
+import org.fundacionkinal.controller.ProductosController;
 
 /**
  *
- * @author informatica
+ * @author Wilson Florian
  */
-//import org.json.JSONObject;
 public class Main extends Application {
 
-    @Override
-    public void start(Stage escenario) throws Exception {
-       FXMLLoader cargadorFXML = new FXMLLoader(getClass().getResource("/view/InicioView.fxml"));
-       Parent raiz = cargadorFXML.load();
-       
-       Scene escena = new Scene(raiz);
-       
-       escenario.setScene(escena);
-       escenario.show();
-    }
+    private Stage escenarioPrincipal;
+    private Scene siguienteEscena;
+    private static String URL = "/org/fundacionkinal/view/";
 
     public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//        JSONObject persona = new JSONObject();
-//        
-//        persona.put("nombre", "Byron");
-//        persona.put("apellido", "Pineda");
-//        persona.put("edad", 17);
-//        persona.put("valido", true);
-//        
-//        System.out.println("Este es el contenido de json");
-//        System.out.println(persona.toString(4));
         launch(args);
-     }
-}
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        this.escenarioPrincipal = escenarioPrincipal;
+        escenarioPrincipal.setTitle("CAJA KINAL");
+        Image icono = new javafx.scene.image.Image("/org/fundacionkinal/image/Logo.png");
+        escenarioPrincipal.getIcons().add(icono);
+        getLoginView();
+        escenarioPrincipal.show();
+    }
+
+    public Initializable cambiarEscena(String fxml, double ancho, double alto) throws Exception {
+        Initializable interfazDeCambio = null;
+        FXMLLoader cargadorFXML = new FXMLLoader();
+        InputStream archivoFXML = Main.class.getResourceAsStream(URL + fxml);
+
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        cargadorFXML.setLocation(Main.class.getResource(URL + fxml));
+
+        siguienteEscena = new Scene(cargadorFXML.load(archivoFXML), ancho, alto);
+        escenarioPrincipal.setScene(siguienteEscena);
+        escenarioPrincipal.sizeToScene();
+
+        interfazDeCambio = cargadorFXML.getController();
+        return interfazDeCambio;
+    }
+
+    public void getLoginView() {
+        try {
+            InicioController control
+                    = (InicioController) cambiarEscena("LoginView.fxml", 1920, 1080);
+            control.setPrincipal(this);
+        } catch (Exception ex) {
+            System.out.println("Error al ir al login: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
